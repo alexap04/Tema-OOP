6a9aa84b6c15246a3b0736a4bea7fd7393fcee34
alexap04 <116150542+alexap04@users.noreply.github.com>
2022-11-24 10:11:20 -0800

    Delete OOP_TEMA1 directory

--- OOP_TEMA1/Lab1_OOP/a.h
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-    int test(int x){
-       x++;
-    return x;
-       }
--- OOP_TEMA1/Lab1_OOP/b.h
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "a.h"
-
-int testNr2(int x){
-    return test(x)*2;
-}
